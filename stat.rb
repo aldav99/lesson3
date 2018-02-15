8192d5cd4a3fa605baaf30716be0b1345cd389ea
aldav99 <aldav99@yandex.ru>
2018-02-15 07:24:45 +0300

    lesson3 try3

--- stat.rb
+++ stat.rb
@@ -1,7 +1,6 @@
 class Station
 
-  attr_reader :name
-  attr_reader :trains
+  attr_reader :name, :trains
   
   def initialize(name)
     @name = name
@@ -12,65 +11,48 @@ class Station
     @trains << train
   end
 
-  def del_train(train)
+  def delete_train(train)
     @trains.delete(train)
   end
 
-  def list_type(type)
-    self.trains.select{ |train| train.type == type }
+  def list_by_type(type)
+    self.trains.select { |train| train.type == type }
   end
-
-  def departure(train)
-     train.go_forward
-  end
-
-  def arrive(train)
-     train.go_back
-  end
-
 end
 
 class Route
   
-  attr_reader :station
+  attr_reader :stations
 
   def initialize(start, terminate)
-    @station = [start, terminate]
+    @stations = [start, terminate]
   end
   
   def start
-    @station[0] 
+    @stations[0] 
   end
 
   def terminate
-    @station[-1]
+    @stations[-1]
   end
 
   def add_station(station)
-    @station.insert(-2, station)
+    @stations.insert(-2, station)
   end
 
-  def del_station(name)
-    if (@station[0].name == name || @station[-1].name == name)
+  def delete_station(name)
+    if (start.name == name || terminate.name == name)
       puts "Начальные точки маршрута нельзя удальять"
     else  
-      @station.delete_if {|station| station.name == name }
+      @stations.delete_if {|station| station.name == name }
     end
   end
-
-  def way
-    puts "Маршрут следования:"
-    @station.each { |station| puts "#{station.name}" }
-  end
-
 end
 
 class Train
 
-  attr_reader :name
-  attr_reader :type
-  attr_reader :speed
-
+  attr_reader :name, :type, :speed
+  
   def initialize(name, type, wagons, route = [])
     @name = name
     @type = type
@@ -79,8 +61,8 @@ class Train
     @route = route
   end
   
-  def change_speed(speed)
-    (@speed + speed) > 0 ? @speed += speed : @speed = 0
+  def change_speed(speed_delta)
+    @speed = [@speed + speed_delta, 0].max
   end
 
   def wagons
@@ -91,56 +73,47 @@ class Train
     @speed = 0
   end
 
-  def attach
+  def attach_wagon
     self.speed > 0 ? "Нужно остановиться" : @wagons += 1
   end
 
-  def deattach
-    puts "Нужно остановиться" if self.speed > 0 
-    @wagons -= 1 if @wagons > 0 
+  def deattach_wagon
+    @wagons -= 1 if @speed == 0 && @wagons > 0 
   end
 
   def route=(route)
     @route = route
-    @location = @route.start
-    @location.add_train(self)
-  end
-
-  def index
-    route = @route.station
-    route.index(@location)
+    @index_location = 0
+    @route.start.add_train(self)
   end
 
   def go_forward
-    return if @location == @route.terminate
-    @location.del_train(self)
-    @location = @route.station[index + 1]
-    @location.add_train(self)
+    return if @route.stations[@index_location] == @route.terminate
+    @route.stations[@index_location].delete_train(self)
+    @index_location += 1
+    @route.stations[@index_location].add_train(self)
   end
 
   def go_back
-    return if @location == @route.start
-    @location.del_train(self)
-    @location = @route.station[index - 1]
-    @location.add_train(self)
+    return if @route.stations[@index_location] == @route.start
+    @route.stations[@index_location].delete_train(self)
+    @index_location -= 1
+    @route.stations[@index_location].add_train(self)
   end
-  
+
   def current_station
-    return @location
+    @route.stations[@index_location]
   end
 
   def back_station
-    return if @location == @route.start
-    back_st = @route.station[index - 1]
-    puts "Предыдущая станция: #{back_st.name}"
+    return if @route.stations[@index_location] == @route.start
+    @route.stations[@index_location - 1]
   end
 
   def next_station
-    return if @location == @route.terminate
-    next_st = @route.station[index + 1]
-    puts "Следующая станция: #{next_st.name}"
+    return if @route.stations[@index_location] == @route.terminate
+    @route.stations[@index_location + 1]
   end
-
 end  
 
 
